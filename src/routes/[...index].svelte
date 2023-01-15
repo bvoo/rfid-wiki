2a8d09415529ad49041036944174b732ed955769
bvoo <60059541+bvoo@users.noreply.github.com>
2023-01-14 23:23:29 -0800

    rfid-wiki initial commit

---
+++ src/routes/[...index].svelte
@@ -0,0 +1,82 @@
+<script lang="ts">
+  import { activeNode } from '$lib/utils';
+  import { createRouter, Router } from '@roxi/routify';
+  import routes from '../../.routify/routes.default';
+  import Navbar from '../components/Navbar.svelte';
+  import Scroller from '../components/Scroller.svelte';
+  import Sidebar from '../components/Sidebar.svelte';
+
+  const router = createRouter({ routes });
+  const rootNode = router.rootNode;
+
+  router &&
+    router.activeRoute.subscribe((activeRoute) => {
+      if (activeRoute) {
+        $activeNode = activeRoute.allFragments[activeRoute.allFragments.length - 1].node;
+      } else {
+        $activeNode = null;
+      }
+    });
+
+  let sidebar = false;
+
+  function toggleSidebar() {
+    sidebar = !sidebar;
+  }
+</script>
+
+<Navbar on:sidebar={toggleSidebar} />
+<div class="popup-bg {sidebar ? '' : 'hidden'}" on:click={() => (sidebar = false)} />
+<main class={sidebar ? '' : 'no-sidebar'}>
+  <div class="sidebar-container {sidebar ? 'open' : 'closed'}">
+    <Scroller>
+      <Sidebar {rootNode} on:clicked={() => (sidebar = false)} />
+    </Scroller>
+  </div>
+  <Scroller>
+    <section class="p-4">
+      <div class="markdown-body">
+        <Router {router} />
+      </div>
+      <div class="divider no-text my-4" />
+      <footer>Footer</footer>
+      <div class="h-12" />
+    </section>
+  </Scroller>
+</main>
+
+<style lang="scss">
+  .sidebar-container {
+    @apply flex;
+
+    width: $sidebar-width;
+
+    @media screen and (max-width: 768px) {
+      position: absolute;
+      top: $header-height;
+      left: 0;
+      bottom: 0;
+
+      &.closed {
+        transform: translateX(-$sidebar-width);
+      }
+    }
+  }
+
+  main {
+    @media screen and (max-width: 768px) {
+      grid-template-columns: 1fr;
+    }
+  }
+
+  .popup-bg {
+    @apply fixed right-0 bottom-0 left-0;
+    top: $header-height;
+
+    @media screen and (max-width: 768px) {
+      @apply bg-black opacity-50;
+    }
+
+    transition: all 0.2s ease-in-out;
+  }
+</style>
---
+++ src/routes/__layout.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+  import '../app.scss';
+
+  import { activeNode, getFriendlyNodeName, kebabToSentenceCase } from '$lib/utils';
+
+  let nodeName = '';
+
+  $: {
+    nodeName = $activeNode ? kebabToSentenceCase(getFriendlyNodeName($activeNode)) : '';
+  }
+</script>
+
+<svelte:head>
+  <title>RFID Wiki{$activeNode ? ` - ${nodeName}` : ''}</title>
+
+  <meta content="en_US" name="locale" />
+  <meta content="#1a1c1d" name="theme-color" />
+  <meta content="yep wiki :) :D" name="description" />
+  <meta content="yep wiki :) :D" property="og:description" />
+  <meta content="yep wiki :) :D" name="twitter:description" />
+
+  <meta content="RFID Wiki{$activeNode ? ` - ${nodeName}` : ''}" property="og:title" />
+  <meta content="RFID Wiki{$activeNode ? ` - ${nodeName}` : ''}" name="twitter:title" />
+  <meta content="RFID Wiki" property="og:site_name" />
+
+  <meta content="website" property="og:type" />
+  <meta content="/favicon.png" property="og:image" />
+  <meta content="/favicon.png" name="twitter:image" />
+  <meta content="summary" name="twitter:card" />
+  <meta content="@kryptonsh" name="twitter:creator" />
+  <meta content="@kryptonsh" name="twitter:site" />
+</svelte:head>
+
+<slot />
